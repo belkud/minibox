00d2ef0afa2ba658a9c700355a6a1b6eadbedf17
belkud <vlad-belkud@yandex.ru>
2024-02-03 12:30:34 +0300

    sdfgf

--- src/main.ts
+++ src/main.ts
@@ -3,52 +3,54 @@ import './style.css'
 let summ = 0
 let deg = 0
 var isPaused = false;
+
+//! Рандомное перемещение
+let color = Math.round(Math.random()*255)
+let color2 = Math.round(Math.random()*255)
+let color3 = Math.round(Math.random()*255)
 const square = document.querySelector('#square') as HTMLDivElement
 square.addEventListener ('click', ()=> {
-  // if (square.style.backgroundColor == 'blue') {
-    //   square.style.backgroundColor = 'red'
-    // } else {
-      //   square.style.backgroundColor = 'blue'
-      // }
-      
-      // square.classList.toggle('blueColor')
-      
+
       if(!isPaused){
         setInterval(()=>{
           square.style.left = `${Math.round(Math.random()*100) + '%'}`
           square.style.top = `${Math.random()*100 + '%'}`
-          let x = deg++
-          square.style.rotate = x*90+'deg'
-          console.log(x);
+          console.log(square.style.left);
+          console.log(square.style.top);
           
-
-          square.innerHTML = `
-          ${summ++} <br>
-          ${square.style.marginLeft}
-          `
-          
-          let color = Math.round(Math.random()*255)
-          let color2 = Math.round(Math.random()*255)
-          let color3 = Math.round(Math.random()*255)
-          
+          let rotatesquare = deg++
+          square.style.rotate = rotatesquare*90+'deg'
+          square.innerHTML = `${summ++}`
+    
           square.style.transition = 1 +'s'
-          square.style.backgroundColor = `rgb(${color}, ${color2}, ${color3})`
-      
-      
+          square.style.background = `linear-gradient(rgb(${color}, ${color2}, ${color3}), rgb(${color2}, ${color3}, ${color}))`
+        },1000)
+        
+        isPaused = true;
+      } else {
+        isPaused = false;
+      }
+    })
+    
+    // console.log(getComputedStyle(document.body).height)
+  
+    
+    //! Перемещение с отскоком
+const ball = document.querySelector('#ball') as HTMLDivElement
+const line = document.querySelector('#line') as HTMLDivElement
+ball.addEventListener ('click', ()=> {
+  setInterval ( ()=>{
+    let digital = Math.round(Math.random()*100) 
+    deg++
+    ball.style.rotate = deg*65 + 'deg'
+    ball.style.left = digital + '%'
+    line.style.left = digital + '%'
+
+    console.log(deg);
+  },3000)
 
 
-    },3000)
-		isPaused = true;
-	} else {
-    isPaused = false;
-	}
-  
-  
 })
 
-// console.log(getComputedStyle(document.body).height)
-// console.log(getComputedStyle(document.body).width)
 
 
-setInterval (()=>{
-},1000)
