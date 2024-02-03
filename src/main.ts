9b5f222b0d3f955fa308ca4c8dbe66c502c94272
belkud <vlad-belkud@yandex.ru>
2024-02-03 19:24:35 +0300

    wwdfg

--- src/main.ts
+++ src/main.ts
@@ -14,16 +14,15 @@ square.addEventListener ('click', ()=> {
           let color = Math.round(Math.random()*255)
           let color2 = Math.round(Math.random()*255)
           let color3 = Math.round(Math.random()*255)
-          square.style.left = `${Math.round(Math.random()*100) + '%'}`
-          square.style.top = `${Math.random()*100 + '%'}`
+          square.style.left = `${Math.round(Math.random()*90) + '%'}`
+          square.style.top = `${Math.round(Math.random()*90) + '%'}`
           console.log(square.style.left);
           console.log(square.style.top);
           
-          let rotatesquare = deg++
-          square.style.rotate = rotatesquare*90+'deg'
+          square.style.rotate = deg++*90+'deg'
           square.innerHTML = `${summ++}`
     
-          square.style.transition = 1 +'s'
+          square.style.transition = 2 +'s'
           square.style.background = `linear-gradient(rgb(${color}, ${color2}, ${color3}), rgb(${color2}, ${color3}, ${color}))`
           console.log(square.style.background);
           
@@ -37,21 +36,24 @@ square.addEventListener ('click', ()=> {
     
     // console.log(getComputedStyle(document.body).height)
   
+
     
     //! Перемещение с отскоком
+
 const ball = document.querySelector('#ball') as HTMLDivElement
 const line = document.querySelector('#line') as HTMLDivElement
+let deg2 = 0
 ball.addEventListener ('click', ()=> {
-  ball.style.animation =  'jump 1s cubic-bezier(0.445, 0.05, 0.55, 0.95) 1s alternate infinite'
+  ball.style.animation =  'jump 2s cubic-bezier(0.445, 0.05, 0.55, 0.95) 2s alternate infinite'
   setInterval ( ()=>{
     let digital = Math.round(Math.random()*100) 
-    deg++
-    ball.style.rotate = deg*65 + 'deg'
+    deg2++
+    ball.style.rotate = deg2*65 + 'deg'
     ball.style.left = digital + '%'
     line.style.left = digital + '%'
 
     console.log(deg);
-  },1000)
+  },4000)
 
 
 })
