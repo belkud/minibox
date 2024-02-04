a83e18add118a1fb2a33493de60e59088933292a
belkud <vlad-belkud@yandex.ru>
2024-02-04 16:29:12 +0300

    vc

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ square.addEventListener ('click', ()=> {
           console.log(square.style.top);
           
           square.style.rotate = deg++*90+'deg'
-          square.innerHTML = `${summ++}`
+          square.innerHTML = `${summ++} steps`
     
           square.style.transition = 2 +'s'
           square.style.background = `linear-gradient(rgb(${color}, ${color2}, ${color3}), rgb(${color2}, ${color3}, ${color}))`
@@ -42,40 +42,64 @@ square.addEventListener ('click', ()=> {
 
 const ball = document.querySelector('#ball') as HTMLDivElement
 const line = document.querySelector('#line') as HTMLDivElement
+const info = document.querySelector('#info') as HTMLDivElement
+const info2 = document.querySelector('#info2') as HTMLDivElement
 let deg2 = 0
+let sec = 0
+
 ball.addEventListener ('click', ()=> {
   ball.style.animation =  'jump 2s cubic-bezier(0.445, 0.05, 0.55, 0.95) 2s alternate infinite'
+  info.style.animation =  'jump 2s cubic-bezier(0.445, 0.05, 0.55, 0.95) 2s alternate infinite'
+  info2.style.animation =  'jump 2s cubic-bezier(0.445, 0.05, 0.55, 0.95) 2s alternate infinite'
   ball.style.rotate = 90 + 'deg'
   // ball.style.transition = 1 + 's'
   setInterval ( ()=>{
     let digital = Math.round(Math.random()*100) 
     deg2++
+   
     ball.style.rotate = deg2*360 + 'deg'
     ball.style.left = digital + '%'
     line.style.left = digital + '%'
+    info.style.left = digital + '%'
+    info2.style.left = digital + '%'
+    info.innerHTML = `
+    количество:
+    оборотов: ${deg2}
+    
+    
+    `
     console.log(ball.style.rotate);
     
   },4000)
+  setTimeout (()=> {
+    setInterval ( ()=> {
+      sec++
+      info2.innerHTML = `
+      отскоков: ${sec} 
+      `
+    },2000)
+
+  },1000)
 }, {once:true})
 
 
 
 
 
+
+
+
+const button = document.querySelector('#button') as HTMLDivElement
 let age = [10, 14, 20, 45, 23, 55, 28]
-const list = document.querySelector('#list') as HTMLDivElement
-// const columns = document.querySelectorAll <HTMLDivElement> ('#columns') 
-
-
 let score = 0
 
 //! через class
 const recs = document.querySelectorAll  ('.rec') as NodeListOf<HTMLDivElement>
 
-list.addEventListener('click', ()=> {
+button.addEventListener('click', ()=> {
   for (let i = 0; i < age.length; i++) {
-    recs[i].style.marginTop = -`${age[i]*2}` + 'px'
-    recs[i].style.transition = 3 + 's'
+    recs[i].style.marginTop = -`${age[i]*3}` + 'px'
+    recs[i].style.transition = 3.5 + 's'
     
     setInterval ( ()=> {
       score+=1      
@@ -86,5 +110,30 @@ list.addEventListener('click', ()=> {
     }, 60)
   }
 })
+
+
+//! через id
+const button2 = document.querySelector('#button2') as HTMLDivElement
+const columns2 = document.querySelectorAll('#columns2') as NodeListOf<HTMLDivElement>
+button2.addEventListener ('click', ()=> {
+  columns2.forEach(columns2 => {
+    columns2.style.color = 'red'
+
+    columns2.style.marginTop = 100+ 'px'
+    for (let i = 0; i < age.length; i++) {
+      // const element = array[i];
+      columns2.style.transition = 1 + 's'
+      
+    }
+
+  });
+  button2.childNodes
+  console.log(button2.childNodes );
+  
+})
+
+
+
+// const columns = document.querySelectorAll <HTMLDivElement> ('#columns') 
 // `${age[i]}`
 // ,{once:true}
