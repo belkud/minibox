0db5ab2ceeff8deb1a024f779ad8d2cb791f40a1
belkud <vlad-belkud@yandex.ru>
2024-02-11 23:58:36 +0300

    sdf

--- src/main.ts
+++ src/main.ts
@@ -131,135 +131,265 @@ button2.addEventListener ('click', ()=> {
 
 
 
+// !Работа с коллекциями ! важно !
+
+
+//! Выведем все дочерние элементы списка .books
+// let booklist = document.querySelector('.books') as HTMLDivElement
+
+// let liveBooks  = booklist.children
+
+// liveBooks [0].remove()
+// console.log(liveBooks.length);
+
+//! Выведем коллекцию, содержащую все элементы с классом book
+let notLiveBooks = document.querySelectorAll(`.book`) 
+
+notLiveBooks[0].remove()
+console.log(Array.from(notLiveBooks));
+
+
+let crossAndZero = document.getElementById('crossAndZero') as HTMLDivElement
+crossAndZero.addEventListener('click', ()=> {
+  console.log(777);
+
+})
+
+ 
 
 // ! Крестики нолики
-const cross = document.querySelector ('#crossAndZero')  as HTMLDivElement
+const symbols = document.querySelectorAll ('#symbols')  
 const player1 = document.querySelector ('#player1')  as HTMLDivElement
 const player2 = document.querySelector ('#player2')  as HTMLDivElement
 const nobody = document.querySelector ('#nobody')  as HTMLDivElement
 
+// const cross = document.querySelector ('#crossAndZero') as HTMLDivElement 
+// console.log(cross.children);
 
-  //! переключение между 'X' и 'О'
-  let move = true
-  move = !move
-  const simbol = {
-    true: 'X',
-    false: 'O'
-  } as any
-console.log(move);
-console.log(simbol[String(move)])
+const cross = document.querySelectorAll ('#crossAndZero') 
+// for (let elem of cross) {
+//   console.log(elem.children);
+  
+//   console.log(elem.children[6].innerHTML = 'X');
+// }
+
+ 
+// cross.forEach(item=>
+//   item.addEventListener('click', ()=> {
+//   for (let i = 0; i < cross.length; i++) {
+//     console.log(symbols.keys);
+    
+    
+//   }
+// }))
 
 
-let score1 = 1
-let score2 = 1
-let score3 = 1
-let step = 0
 
 
-cross.addEventListener('click', (event)=>{
-  let num = cross.children
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// const cross = document.querySelector ('#crossAndZero')  as HTMLDivElement
+// const player1 = document.querySelector ('#player1')  as HTMLDivElement
+// const player2 = document.querySelector ('#player2')  as HTMLDivElement
+// const nobody = document.querySelector ('#nobody')  as HTMLDivElement
+
+
+
+//   //! переключение между 'X' и 'О'
+//   let move = true
+//   move = !move
+//   const simbol = {
+//     true: 'X',
+//     false: 'O'
+//   } as any
+// console.log(move);
+// console.log(simbol[String(move)])
+
+
+// let score1 = 1
+// let score2 = 1
+// let score3 = 1
+// let step = 0
+
+
+// cross.addEventListener('click', (event)=>{
+//   let num = cross.children
 
   
     
 
-    if (simbol[String(move)]=='X') {
-      simbol[String(move)]='O'
-      // console.log(1);
+//     if (simbol[String(move)]=='X') {
+//       simbol[String(move)]='O'
+//       // console.log(1);
       
-    } else {
-      simbol[String(move)]='X'
-      // console.log(2);
-    }
+//     } else {
+//       simbol[String(move)]='X'
+//       // console.log(2);
+//     }
    
-  let cell = event.target as HTMLDivElement
-  cell.innerHTML = simbol[String(move)]
-  cell.classList.add('lightGreen')
-  if (cell.innerHTML = simbol[String(move)]){
-    cell.style.pointerEvents = 'block'
-    console.log(555);
-  }
-   
-
-  if (num[0].innerHTML=='X' && num[1].innerHTML=='X' && num[2].innerHTML=='X' ||
-  num[3].innerHTML=='X' && num[4].innerHTML=='X' && num[5].innerHTML=='X' ||
-  num[6].innerHTML=='X' && num[7].innerHTML=='X' && num[8].innerHTML=='X' ||
-  num[0].innerHTML=='X' && num[3].innerHTML=='X' && num[6].innerHTML=='X' ||
-  num[1].innerHTML=='X' && num[4].innerHTML=='X' && num[7].innerHTML=='X' ||
-  num[2].innerHTML=='X' && num[5].innerHTML=='X' && num[8].innerHTML=='X' ||
-  num[0].innerHTML=='X' && num[4].innerHTML=='X' && num[8].innerHTML=='X' ||
-  num[2].innerHTML=='X' && num[4].innerHTML=='X' && num[6].innerHTML=='X'
-  ) {
-    setTimeout(() => {
-      alert('Поздравляем игрок 1 выиграл');
-      for (let i = 0; i < 9; i++) {
-        cross.children[i].innerHTML = ''        
-        cross.children[i].classList.remove('lightGreen')
-      }
-      player1.innerHTML = `${score1++}`
-    }, 100);
-  }
-  
-  else if (num[0].innerHTML=='O' && num[1].innerHTML=='O' && num[2].innerHTML=='O' ||
-  num[3].innerHTML=='O' && num[4].innerHTML=='O' && num[5].innerHTML=='O' ||
-  num[6].innerHTML=='O' && num[7].innerHTML=='O' && num[8].innerHTML=='O' ||
-  num[0].innerHTML=='O' && num[3].innerHTML=='O' && num[6].innerHTML=='O' ||
-  num[1].innerHTML=='O' && num[4].innerHTML=='O' && num[7].innerHTML=='O' ||
-  num[2].innerHTML=='O' && num[5].innerHTML=='O' && num[8].innerHTML=='O' ||
-  num[0].innerHTML=='O' && num[4].innerHTML=='O' && num[8].innerHTML=='O' ||
-  num[2].innerHTML=='O' && num[4].innerHTML=='O' && num[6].innerHTML=='O'
-  ) {
-    setTimeout(() => {
-      alert('Поздравляем игрок 2 выиграл');
-      for (let i = 0; i < 9; i++) {
-        cross.children[i].innerHTML = ''
-        cross.children[i].classList.remove('lightGreen')
-      }
-      player2.innerHTML = `${score2++}`
-    }, 100);
-  }
-  else {
-    step++
-    setTimeout(() => {
-      if (step>=9) {
-        alert('Ничья');
-        for (let i = 0; i < 9; i++) {
-                cross.children[i].innerHTML = ''
-                cross.children[i].classList.remove('lightGreen')
-                // document.location.reload()
-                step=0
-              }
-      }
-
-      
-      nobody.innerHTML = `${score3++}`
-    }, 100);
+//   let cell = event.target as HTMLDivElement
+//   cell.innerHTML = simbol[String(move)]
+//   cell.classList.add('lightGreen')
+//   if (cell.innerHTML = simbol[String(move)]){
+//     console.log(555);
+//   }
+//   if(cell.innerHTML=='X') {
+//     // cell.style.pointerEvents = 'none'
     
-  }
-  console.log(step);
+//   }
+
+//   if (num[0].innerHTML=='X' && num[1].innerHTML=='X' && num[2].innerHTML=='X' ||
+//   num[3].innerHTML=='X' && num[4].innerHTML=='X' && num[5].innerHTML=='X' ||
+//   num[6].innerHTML=='X' && num[7].innerHTML=='X' && num[8].innerHTML=='X' ||
+//   num[0].innerHTML=='X' && num[3].innerHTML=='X' && num[6].innerHTML=='X' ||
+//   num[1].innerHTML=='X' && num[4].innerHTML=='X' && num[7].innerHTML=='X' ||
+//   num[2].innerHTML=='X' && num[5].innerHTML=='X' && num[8].innerHTML=='X' ||
+//   num[0].innerHTML=='X' && num[4].innerHTML=='X' && num[8].innerHTML=='X' ||
+//   num[2].innerHTML=='X' && num[4].innerHTML=='X' && num[6].innerHTML=='X'
+//   ) {
+//     setTimeout(() => {
+//       alert('Поздравляем игрок 1 выиграл');
+//       for (let i = 0; i < 9; i++) {
+//         cross.children[i].innerHTML = ''        
+//         cross.children[i].classList.remove('lightGreen')
+//       }
+//       player1.innerHTML = `${score1++}`
+//     }, 100);
+//   }
   
-      })
+//   else if (num[0].innerHTML=='O' && num[1].innerHTML=='O' && num[2].innerHTML=='O' ||
+//   num[3].innerHTML=='O' && num[4].innerHTML=='O' && num[5].innerHTML=='O' ||
+//   num[6].innerHTML=='O' && num[7].innerHTML=='O' && num[8].innerHTML=='O' ||
+//   num[0].innerHTML=='O' && num[3].innerHTML=='O' && num[6].innerHTML=='O' ||
+//   num[1].innerHTML=='O' && num[4].innerHTML=='O' && num[7].innerHTML=='O' ||
+//   num[2].innerHTML=='O' && num[5].innerHTML=='O' && num[8].innerHTML=='O' ||
+//   num[0].innerHTML=='O' && num[4].innerHTML=='O' && num[8].innerHTML=='O' ||
+//   num[2].innerHTML=='O' && num[4].innerHTML=='O' && num[6].innerHTML=='O'
+//   ) {
+//     setTimeout(() => {
+//       alert('Поздравляем игрок 2 выиграл');
+//       for (let i = 0; i < 9; i++) {
+//         cross.children[i].innerHTML = ''
+//         cross.children[i].classList.remove('lightGreen')
+//       }
+//       player2.innerHTML = `${score2++}`
+//     }, 100);
+//   }
+//   else {
+//     step++
+//     setTimeout(() => {
+//       if (step>=9) {
+//         alert('Ничья');
+//         for (let i = 0; i < 9; i++) {
+//                 cross.children[i].innerHTML = ''
+//                 cross.children[i].classList.remove('lightGreen')
+//                 // document.location.reload()
+//                 step==0
+//               }
+//               nobody.innerHTML = `${score3++}`
+//       }
+
+      
+//     }, 100);
+    
+//   }
+//   console.log(step);
+  
+//       })
        
 
 
 
 
 
+const letter = document.querySelector('#letter') as HTMLDivElement
 
 
 
 
-const changeDigital = document.querySelector('.mainButton') as HTMLDivElement
-changeDigital.addEventListener ('click',(event)=> {
-  console.log(event.target);
-  let x = changeDigital.style
 
-  if (x.backgroundColor == 'red') {
-    x.backgroundColor = 'blue'
-    
-    
-  } else {
-    x.backgroundColor = 'red'
-  }
+const changeDigital = document.querySelectorAll('.extraButton') 
+// const changeDigital = document.querySelector('.mainButton') 
+let scores = 0 
+
+for (let elem of changeDigital) {
   
-})
+function check() {
+  if (scores%2==0) {
+    elem.innerHTML = 'X'
+  } else {
+    elem.innerHTML = 'Y'
+  }
+  scores++
+  if (scores==4) {
+    console.log('ничья!');
+    setTimeout(() => {
+      document.location.reload()
+      
+    }, 500);
+    // scores =0
+  }
+  elem.removeEventListener('click', check)
+  console.log(scores);
+  letter.innerHTML=  `${scores}`
+  // changeDigital.
+}
+
+
+elem.addEventListener ('click', check)
+
+}
+
+ let peremennay = document.querySelectorAll('.extraButton') as NodeList
+
+  for (let i = 0; i < peremennay.length; i++) {
+      peremennay[i].innerHTML = ''  
+  }
+
+
+
+
+
+
+
+// let btn  = document.getElementById('idButton') as HTMLElement 
+// btn.addEventListener ('click', ()=> {
+//   for (let i = 0; i < btn.childElementCount; i++) {
+//     if (scores==4)
+//     btn.children[i].innerHTML = '5'
+//   console.log('hi');
+//   console.log(scores);
+  
+//   }
+
+
+   
+//   // btn.innerHTML = '7'
+  
+// })
+
+
+
+
+
+
 
